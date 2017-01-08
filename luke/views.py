a850baa477ab706eff77338ee0b00455813f5b83
Luke Higgott <higgottluke@gmail.com>
2017-01-08 00:35:23 -0800

    more stuff!

--- luke/views.py
+++ luke/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import BlogPost
 
 # Create your views here.
 def home(request):
@@ -7,3 +9,6 @@ def home(request):
 def about(request):
 	return render(request, 'about.html')
 
+def blog_home(request):
+	posts = BlogPost.objects.all().order_by('published_date')
+	return render(request, 'blog_list.html', {'posts': posts})
