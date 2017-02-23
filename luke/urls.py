f1bcdb74336d980d8a426af36d498a257e81f63e
Luke Higgott <higgottluke@gmail.com>
2017-02-23 10:06:46 -0800

    changed urlconf for filtering blog and project posts

--- luke/urls.py
+++ luke/urls.py
@@ -6,7 +6,9 @@ urlpatterns = [
 	url(r'^about/$', views.about, name='about'),
 	url(r'^blog/$', views.blog_home, name='blog'),
 	url(r'^blog/([a-zA-Z0-9-\']*)/$', views.blog_post, name='blog_post'),
+	url(r'^blog/tag/([a-zA-Z0-9-\']*)/$', views.blog_home, name='blog_by_tag'),
 	url(r'^projects/$', views.projects_home, name='projects_list'),
 	url(r'^projects/([a-zA-Z0-9-\']*)/$', views.project, name='project'),
+	url(r'^projects/tag/([a-zA-Z0-9-\']*)/$', views.projects_home, name='projects_by_tag'),
 	url(r'^contact/$', views.contact, name='contact')
 ]
