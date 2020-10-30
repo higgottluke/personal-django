d9d62da518d84e2e8dd445a12bb4270b599341a8
Luke Higgott <higgottluke@gmail.com>
2020-10-30 12:36:21 -0700

    draft material for geol201L project

--- geology/views.py
+++ geology/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'geol-home.html')
 
 
 def about(request):
     return render(request, "about.html")
+
+
+def explore(request):
+    return render(request, "explore.html")
